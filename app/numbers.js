d35126bb7a2c7142baeb0a3cf2694feb73a60692
benharrison <bharrison912@gmail.com>
2015-11-30 16:31:53 -0500

    operators and numbers

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,20 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    return 1 & (num >> (bit - 1));
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
-
+    for (var nFlag = 0, nShifted = num, sMask = ""; nFlag < 32;
+        nFlag++, sMask += String(nShifted >>> 31), nShifted <<= 1);
+    return sMask.substr(-8);
   },
 
   multiply: function(a, b) {
-
+    return a * b;
   }
 };
