60d381e1ef25170578353a0918f043d400d5dccc
benharrison <bharrison912@gmail.com>
2015-11-30 13:13:19 -0500

    initial commit

--- app/flowControl.js
+++ app/flowControl.js
@@ -11,5 +11,13 @@ exports.flowControlAnswers = {
     // otherwise the function should return the number, or false if no number
     // was provided or the value provided is not a number
 
+    if (isNaN(num)) return false;
+
+    var output = '';
+    if (num % 3 == 0) output += 'fizz';
+    if (num % 5 == 0) output += 'buzz';
+    if (output.length > 0) return output;
+
+    return num;          
   }
 };
