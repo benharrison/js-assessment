f5ffb3613381bc911b93d6bf4e437ad990337c02
benharrison <bharrison912@gmail.com>
2015-11-30 13:07:37 -0500

    initial commit

--- app/count.js
+++ app/count.js
@@ -2,6 +2,20 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
-
+    var timeout;
+    function sum() {
+      console.log(start++);
+      if (start <= end)
+        timeout = setTimeout(sum, 100);
+    }
+    sum();
+    
+    return {
+      cancel: function() {
+        if (timeout)
+          clearTimeout(timeout);
+      }
+    };
+    
   }
 };
