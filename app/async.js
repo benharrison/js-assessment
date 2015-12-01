9f26673a2c70bcbf9a568eba9005e9ba3a2e9a63
benharrison <bharrison912@gmail.com>
2015-11-30 21:33:03 -0500

    completing async and module pattern

--- app/async.js
+++ app/async.js
@@ -2,10 +2,29 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    var dfd = $.Deferred();
+    
+    dfd.resolve(value);
+    
+    return dfd.promise();
   },
 
   manipulateRemoteData : function(url) {
+    var dfd = $.Deferred();
 
+    var ajax = $.ajax({
+        url: url
+      });
+      
+    ajax.done(function(resp) {
+      var people = [];
+      $.each(resp.people, function(index, obj){
+        people.push(this.name);
+      });
+      
+      dfd.resolve(people.sort());
+    });
+
+    return dfd.promise();  
   }
 };
--- app/recursion.js
+++ app/recursion.js
@@ -6,7 +6,22 @@ exports.recursionAnswers = {
   },
 
   permute: function(arr) {
-
+    var permArr = new Array();
+    var usedChars = new Array();
+    
+    return (function main(){
+      for (var i = 0, len = arr.length; i < len; i++)
+      {
+        var ch = arr.splice(i, 1)[0];
+        usedChars.push(ch);
+        if (len == 0) {
+          permArr.push(usedChars.slice());
+        }
+        main();
+        input.splice(i, 0, ch);
+        usedChars.pop();
+      }
+    })();
   },
 
   fibonacci: function(n) {
