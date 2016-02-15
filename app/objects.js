e621dd8431b1c1a0444bbb4134e6a0440f73d433
benharrison <bharrison912@gmail.com>
2016-02-15 14:40:27 -0500

    objects

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,20 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   alterObjects : function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate : function(obj) {
-
+    var result = [];
+    for (p in obj)
+    {
+        if (obj.hasOwnProperty(p))
+            result.push(p + ': ' + obj[p]);
+    }
+    return result;
   }
 };
